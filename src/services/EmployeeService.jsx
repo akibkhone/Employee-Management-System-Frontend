858919d058f78211a746f39481d9c75f0c9afb0f
akibkhone <akibkhone@gmail.com>
2024-03-02 01:03:49 +0530

    Title Update

--- src/services/EmployeeService.jsx
+++ src/services/EmployeeService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EMPLOYEE_API_BASE_URL = "https://employee-management-system-backend-hfxp.onrender.com/api/v1";
+const EMPLOYEE_API_BASE_URL = "https://employeemanagementsystem-latest.onrender.com/api/v1";
 
 class EmployeeService {
   saveEmployee(employee) {
